547ae4613d7e9842deca6c84a88283920ea65b19
Hao LI <94358665+Hao-Li-lih@users.noreply.github.com>
2022-10-13 09:47:24 +0200

    add TCP/IP Server

---
+++ src/main/scala/client.scala
@@ -0,0 +1,23 @@
+import java.net.ServerSocket
+import java.net.Socket
+import java.io.PrintWriter
+import scala.util.Using
+
+
+object ClientMain {
+  def main(args: Array[String]): Unit =
+    Using(new Socket("localhost", ServerMain.port)) { server =>
+      println(s">>> connected to server ${server.getInetAddress}")
+
+      println(">>> sending request...")
+      val printer = new PrintWriter(server.getOutputStream)
+      printer.println("Jon")
+      printer.flush()
+
+      println(">>> waiting for a response...")
+      val responseStream = Source.fromInputStream(server.getInputStream)
+      for (elem <- responseStream.getLines())
+        println(elem)
+    }.get
+}
+
