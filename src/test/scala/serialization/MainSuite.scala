ffb69fa0a5c3699f79fe2242d64f641ca43df8af
Yijun LIU <yijun.liu@edu.esiee.fr>
2022-10-13 10:41:41 +0200

    add test

--- src/test/scala/serialization/MainSuite.scala
+++ src/test/scala/serialization/MainSuite.scala
@@ -1,9 +1,13 @@
+package serialization
+
+import serialization.PseudobinSerde
+
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class MainSuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
+  test("INT to Pseudobin") {
+    val obtained = PseudobinSerde.INT.serialize(42)
+    val expected = "         42"
     assertEquals(obtained, expected)
   }
 }
