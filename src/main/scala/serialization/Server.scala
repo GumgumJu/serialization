4633c92dd603388cfa2b70f5eb17745537779292
Hao LI <94358665+Hao-Li-lih@users.noreply.github.com>
2022-10-13 10:46:14 +0200

    Update Server.scala
    Server V1

--- src/main/scala/serialization/Server.scala
+++ src/main/scala/serialization/Server.scala
@@ -1,22 +1,54 @@
 package serialization
 
-import java.io.{BufferedReader, PrintWriter}
-import java.net.{ServerSocket, Socket}
+import java.net.ServerSocket
+import java.net.Socket
+import java.io.{BufferedReader, InputStreamReader, PrintWriter}
 import scala.util.Using
-
+import scala.io.Source
 
 object ServerMain {
-  def main(args: Array[String]): Unit = {
-    val serverSocket = new ServerSocket(9071);
+  val port = 9071
 
-      val socket = serverSocket.accept();
-      val bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream, "utf-8"));
+  def main(args: Array[String]): Unit =
+  // server-side socket creation
+    Using(new ServerSocket(port)) { server =>
+      println(s">>> serve on port $port")
 
-      val data = bufferedReader.readLine();
-      println(data);
+      // loop forever, as it is a service
+      while (true)
+      // wait for a connection from a client
+        Using(server.accept()) { client =>
+          processDataFrom(client)
+        }.fold( // fold acts here like an if
+          // if we have an error with the client...
+          error => println(s">>> client connection failure: ${error.getMessage}"),
+          // otherwise, we do nothing
+          _ => ()
+        )
+    }.get // calling "get" here might throw an exception if there is an error
 
-      bufferedReader.close();
+  def processDataFrom(client: Socket): Unit = {
+    println(s">>> connected with a client")
 
-      socket.close();
-      }
-      }
+    val name: String = readDataFrom(client).next()
+    println(s">>> got a name: $name")
+
+    println(s">>> sending a response")
+    sendDataTo(client, s"Hello $name!")
+  }
+
+  def readDataFrom(client: Socket): Iterator[String] = {
+    // get data sent by the client to the server
+    val source = Source.fromInputStream(client.getInputStream)
+
+    source.getLines()
+  }
+
+  def sendDataTo(client: Socket, data: String): Unit = {
+    val printer = new PrintWriter(client.getOutputStream)
+    printer.println(data)
+    // flush is necessary to ensure that is sent to the client
+    printer.flush()
+  }
+
+}
