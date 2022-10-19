e72480d92f8b82227200ef294adc504f3921d9ef
Hao LI <94358665+Hao-Li-lih@users.noreply.github.com>
2022-10-19 21:22:23 +0200

    Update Server
    1 question about message（java to scala）

--- src/main/scala/serialization/Server.scala
+++ src/main/scala/serialization/Server.scala
@@ -6,49 +6,32 @@ import java.io.{BufferedReader, InputStreamReader, PrintWriter}
 import scala.util.Using
 import scala.io.Source
 
-object ServerMain {
-  val port = 9071
-
-  def main(args: Array[String]): Unit =
-  // server-side socket creation
-    Using(new ServerSocket(port)) { server =>
-      println(s">>> serve on port $port")
-
-      // loop forever, as it is a service
-      while (true)
-      // wait for a connection from a client
-        Using(server.accept()) { client =>
-          processDataFrom(client)
-        }.fold( // fold acts here like an if
-          // if we have an error with the client...
-          error => println(s">>> client connection failure: ${error.getMessage}"),
-          // otherwise, we do nothing
-          _ => ()
-        )
-    }.get // calling "get" here might throw an exception if there is an error
-
-  def processDataFrom(client: Socket): Unit = {
-    println(s">>> connected with a client")
-
-    val name: String = readDataFrom(client).next()
-    println(s">>> got a name: $name")
-
-    println(s">>> sending a response")
-    sendDataTo(client, s"Hello $name!")
+object TcpServer {
+    @throws[IOException]
+    def main(args: Array[String]): Unit = {
+      val port = 9071
+      val connectionSocket = new ServerSocket(port)
+      // 监听端口，accept为阻塞方法
+      System.out.println("Get socket successfully, wait for request...")
+      val communicationSocket = connectionSocket.accept
+      // 获取输入流，读取数据（此处有问题）
+      val inputStream = communicationSocket.getInputStream
+      val inputStreamReader = new InputStreamReader(inputStream)
+      val bufferedReader = new BufferedReader(inputStreamReader)
+      var message = null
+        while ( {
+                 (message = bufferedReader.readLine) != null
+           }) System.out.printf("get message from client : %s", message)
+      communicationSocket.shutdownInput()
+      // 获取输出流,返回结果
+      val outputStream = communicationSocket.getOutputStream
+      val outputStreamWriter = new OutputStreamWriter(outputStream)
+      val bufferedWriter = new BufferedWriter(outputStreamWriter)
+      bufferedWriter.write("收到，结束！")
+      bufferedWriter.flush()
+      communicationSocket.shutdownOutput()
+      // close
+      bufferedWriter.close()
+      bufferedReader.close()
+    }
   }
-
-  def readDataFrom(client: Socket): Iterator[String] = {
-    // get data sent by the client to the server
-    val source = Source.fromInputStream(client.getInputStream)
-
-    source.getLines()
-  }
-
-  def sendDataTo(client: Socket, data: String): Unit = {
-    val printer = new PrintWriter(client.getOutputStream)
-    printer.println(data)
-    // flush is necessary to ensure that is sent to the client
-    printer.flush()
-  }
-
-}
