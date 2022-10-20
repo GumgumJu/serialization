957bf347d157c37099d8b886fef3f90eab117108
Hao LI <94358665+Hao-Li-lih@users.noreply.github.com>
2022-10-20 09:37:49 +0200

    client last time

--- src/main/scala/serialization/client.scala
+++ src/main/scala/serialization/client.scala
@@ -5,23 +5,27 @@ import java.net.Socket
 import java.io.PrintWriter
 import scala.io.Source
 import scala.util.Using
+import scala.io._
 
-
-
-object ClientMain {
+object Client {
+  
   def main(args: Array[String]): Unit =
-    Using(new Socket("localhost", ServerMain.port)) { server =>
+    Using(new Socket("localhost", Server.port)) { server =>
       println(s">>> connected to server ${server.getInetAddress}")
 
       println(">>> sending request...")
+      println(">>> serialization...")
+      val line = StdIn.readLine()
+      println("you input is: " + line)
+      println(">>> deserialization...")
+      val deline = StdIn.readLine()
+      println("you input is: " + deline)
       val printer = new PrintWriter(server.getOutputStream)
-      printer.println("Jon")
+     printer.println(line)
       printer.flush()
-
       println(">>> waiting for a response...")
       val responseStream = Source.fromInputStream(server.getInputStream)
       for (elem <- responseStream.getLines())
         println(elem)
     }.get
 }
-
